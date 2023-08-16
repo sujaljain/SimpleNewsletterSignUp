79bb66b1b05e943b3626d3a6096e2000ae9fb686
Sujal Jain <68064961+sujaljain@users.noreply.github.com>
2023-08-16 16:37:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+// Import required libraries
+const express = require('express'); // Import the Express framework
+const bodyParser = require('body-parser'); // Middleware for parsing request bodies
+const mailgun = require('mailgun-js'); // Library for sending emails
+
+// Create an instance of the Express app
+const app = express();
+
+// Middleware setup
+app.use(bodyParser.urlencoded({ extended: false })); // Use body-parser middleware for parsing URL-encoded request bodies
+app.use(express.static('public')); // Serve static files from the 'public' directory
+
+// Home page route
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html'); // Send the 'index.html' file as the response
+});
+
+// Subscription endpoint
+app.post('/subscribe', (req, res) => {
+    // Extract the email from the request body
+    const { email } = req.body;
+
+    // Mailgun API credentials
+    const api_key = 'key-f744750ef9827ca37cad82999c7268e3';
+    const domain = 'sandbox4187c8b5a04e4b7083719345ccf89b3f.mailgun.org';
+
+    // Create a Mailgun instance
+    const mg = mailgun({ apiKey: api_key, domain: domain });
+
+    // Email data to be sent
+    const emailData = {
+        from: 'sujal4845.be22@chitkara.edu.in',
+        to: email,
+        subject: 'Welcome OnBoard!!',
+        html: `<p>Hi ${email},</p>
+               <p>Thank you for subscribing to our newsletter!</p>
+               <p>We hope you enjoy our service.</p>
+               <p>Have a good day!</p>`
+    };
+
+    // Send the email using Mailgun API
+    mg.messages().send(emailData, (error, body) => {
+        if (error) {
+            console.error('Error sending email:', error);
+            res.status(500).send('Error sending email'); // Respond with an error if sending fails
+        } else {
+            console.log('Email sent:', body);
+            res.send('Subscription successful! Check your email.'); // Respond with success message
+        }
+    });
+});
+
+// Start the server
+const port = process.env.PORT || 3000; // Use the specified environment port or default to 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`); // Log server start
+});
